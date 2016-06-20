fa6f1734007dcbcf2de3f1ba8397c86bec2063dc
louis <dsawcss@hotmail.com>
2016-06-20 21:01:50 +0800

    a

--- Dynamic/WebContent/Website Files/Home.js
+++ Dynamic/WebContent/Website Files/Home.js
@@ -1,2 +1,9 @@
 
+<meta http-equiv="refresh" content="0; url=http://www.socialfoodinc.com/">
 
+window.location = "http://www.socialfoodinc.com";
+
+window.location = "http://www.socialfoodinc.com";
+window.location.href = "http://www.socialfoodinc.com";
+window.location.assign("http://www.socialfoodinc.com");
+window.location.replace("http://www.socialfoodinc.com");
