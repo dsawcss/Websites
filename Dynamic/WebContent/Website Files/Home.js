21745af585d97e76f2142d941387769f00c26972
louis <dsawcss@hotmail.com>
2016-11-03 00:05:50 +0800

    a

--- Dynamic/WebContent/Website Files/Home.js
+++ Dynamic/WebContent/Website Files/Home.js
@@ -1,7 +1,8 @@
 function myFunction() {
-  var x = document.getElemntById("myNavBar");
-  if (x.className === "NavBar") {
-      x.className +=" responsive";
-  } else {
-      x.className = "NavBar";}
+    var x = document.getElementById("MyNavBar");
+    if (x.className === "NavBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "NavBar";
+    }
 }
