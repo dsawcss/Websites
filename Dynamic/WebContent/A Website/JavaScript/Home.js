b782b81b926099d930094f84449de20229e077ef
louis <dsawcss@hotmail.com>
2016-12-30 23:34:06 +0800

    a

--- Dynamic/WebContent/A Website/JavaScript/Home.js
+++ Dynamic/WebContent/A Website/JavaScript/Home.js
@@ -1,8 +1,23 @@
-function ResponsiveNav() {
-  var x = document.getElementById("MyNavBar");
-  if (x.className === "NavBar") {
-      x.className += " responsive";
+/*
+  Long form of the Below Function
+  function ResponsiveNav() {
+  var x = document.getElementById("MyNavBar"); // Cannot Select Class By Id
+  x.className += " responsive" // adding additonal class to an element
+  if (x.className === "NavBar responsive") {
+      x.className = "NavBar responsive";   
   } else {
       x.className = "NavBar";
       }
 }
+*/
+
+//short form of the function
+function ResponsiveNav() {
+  var x = document.getElementById("MyNavBar");
+  if (x.className === "NavBar") {
+      x.className += " responsive"; 
+  } else {
+      x.className = "NavBar";
+      }
+}
+
