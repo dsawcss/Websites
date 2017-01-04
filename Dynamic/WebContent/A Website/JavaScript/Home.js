11be516a1beb7faad2a9b9d1bbc09281932ed9c3
louis <dsawcss@hotmail.com>
2017-01-05 00:51:49 +0800

    a

--- Dynamic/WebContent/A Website/JavaScript/Home.js
+++ Dynamic/WebContent/A Website/JavaScript/Home.js
@@ -6,12 +6,12 @@
   if (x.className === "TopNav Responsive") { // triple equal to prevent type change
       x.className = "TopNav Responsive";   
   } else {
-      x.className = "TopNav";
+      x.className = "TopNav"; // to return back to original element
       }
 }
 */
 
-//function to open dropdown and return to original popsition
+//function to open dropdown and return to original element
 function ResponsiveNav() {
   var x = document.getElementById("MyTopNav");
   if (x.className === "TopNav") {
