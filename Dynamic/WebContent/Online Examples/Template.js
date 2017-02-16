85606fd5b9845179b9c111328f27067aa9e378fc
louis <dsawcss@hotmail.com>
2017-02-16 21:41:31 +0800

    a

---
+++ Dynamic/WebContent/Online Examples/Template.js
@@ -0,0 +1,22 @@
+/*
+  Long form of the Below Function
+  function ResponsiveNav() {
+  var x = document.getElementById("MyTopNav"); // Cannot Select Class Only By Id
+  x.className += " Responsive" // adding additonal class to an element
+  if (x.className === "TopNav Responsive") { // triple equal to prevent type change
+      x.className = "TopNav Responsive";   
+  } else {
+      x.className = "TopNav"; // to return back to original element
+      }
+}
+*/
+
+//function to open dropdown and return to original element
+function ResponsiveNav() {
+	var x = document.getElementById("MyTopNav");
+	if (x.className === "TopNav") {
+		x.className += " Responsive"; 
+	} else {
+		x.className = "TopNav";
+	}
+}
