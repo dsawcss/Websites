3720a60eb71f3b50b5f563f7b350bfa28ff79649
louis <dsawcss@hotmail.com>
2017-02-18 00:16:08 +0800

    a

--- Dynamic/WebContent/A Website/JavaScript/Template.js
+++ Dynamic/WebContent/A Website/JavaScript/Template.js
@@ -1,18 +1,23 @@
-/*
-Long form of the Below Function
-function ResponsiveNav() {
-var x = document.getElementById("MyTopNav"); // Cannot Select Class Only By Id
-x.className += " Responsive" // adding additonal class to an element
-if (x.className === "TopNav Responsive") { // triple equal to prevent type change
-    x.className = "TopNav Responsive";   
-} else {
-    x.className = "TopNav"; // to return back to original element
+/*cannot use php for responsive websites
+ * used to detect screen width and height for responsive websites
+ * log for debugger
+ */
+screenWidth = window.screen.width,
+    screenHeight = window.screen.height;
+    console.log(screenWidth);
+    console.log(screenHeight);
+    /*
+    Long form of the Below Function
+    function ResponsiveNav() {
+    var x = document.getElementById("MyTopNav"); // Cannot Select Class Only By Id
+    x.className += " Responsive" // adding additonal class to an element
+    if (x.className === "TopNav Responsive") { // triple equal to prevent type change
+        x.className = "TopNav Responsive";   
+    } else {
+        x.className = "TopNav"; // to return back to original element
+        }
     }
-}
-*/
-var width = window.innerWidth;
-    document.write('<iframe src="content.php?w='+width+'"></iframe>');
-    
+    */
 //function to open dropdown and return to original element
 function ResponsiveNav() {
   var x = document.getElementById("MyTopNav");
