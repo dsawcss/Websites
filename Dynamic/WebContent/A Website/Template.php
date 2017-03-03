2826b4bd3d10f05f5f47e65b5508800a83770adf
louis <louis@192.168.1.7>
2017-03-03 23:35:04 +0800

    aa

--- Dynamic/WebContent/A Website/Template.php
+++ Dynamic/WebContent/A Website/Template.php
@@ -29,7 +29,7 @@
     </div>
     
     <ul class="TopNav" id="MyTopNav">
-        <li><a class="active" href="Index.php" title="Index">Home</a></li>
+        <li><a class="active" href="Index.php" title="Index">Testing</a></li>
         <li><a href="#" title="Computer">Computer</a></li>
         <li><a href="#" title="Phone">Phone</a></li>
         <li><a href="#" title="Tablets">Tablets</a></li>
