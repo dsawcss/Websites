7df0490d711439cf57054d2d80c60a28f91e2867
louis <louis@192.168.1.7>
2017-03-08 23:06:54 +0800

    a

--- Dynamic/WebContent/A Website/JavaScript/Template.js
+++ Dynamic/WebContent/A Website/JavaScript/Template.js
@@ -9,7 +9,7 @@ console.log(screenHeight);
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = $('header').outerHeight();
+var navbarHeight = $('.header').outerHeight();
 
 $(window).scroll(function(event){
     didScroll = true;
@@ -31,11 +31,11 @@ function hasScrolled() {
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight){
         // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
+        $('.header div').removeClass('nav-down').addClass('header');
     } else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
+            $('.header div').removeClass('header').addClass('nav-down');
         }
     }
     
