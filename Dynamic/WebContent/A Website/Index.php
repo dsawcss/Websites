583257d68155da48deb3060d3f3e48a8e21901bb
louis <louis@192.168.1.1>
2017-07-07 23:12:51 +0800

    a

--- Dynamic/WebContent/A Website/Index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$title = "Home";
-$content = '<p>testing</p>';
-$second = '<p> test2</p>';
-
-include 'Template.php';
-?>
--- Dynamic/WebContent/A Website/Model/Credentials.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-//Login data for the testing database. Not to be used for actual website
-$host = "localhost";
-$user = "root";
-$password = "root";
-$database = "test";
-
-?>
