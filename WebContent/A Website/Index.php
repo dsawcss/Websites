db7305cc329045194cf261e2f2568649145ca544
louis <louis@192.168.1.1>
2017-07-07 23:11:42 +0800

    a

---
+++ WebContent/A Website/Index.php
@@ -0,0 +1,8 @@
+<?php 
+
+$title = "Home";
+$content = '<p>testing</p>';
+$second = '<p> test2</p>';
+
+include 'Template.php';
+?>
---
+++ WebContent/A Website/Model/Credentials.php
@@ -0,0 +1,9 @@
+<?php 
+
+//Login data for the testing database. Not to be used for actual website
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database = "test";
+
+?>
